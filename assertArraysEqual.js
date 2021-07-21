e0da958331f71e77373628759f7a386e50cdba27
chandni <chandnivaghasia11@gmail.com>
2021-07-20 21:38:11 -0400

    assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,30 @@
+function eqArrays(arr1, arr2)
+{
+  if (arr1.length !== arr2.length) return false;
+  for (var i = 0, len = arr1.length; i < len; i++){
+      if (arr1[i] !== arr2[i]){
+          return false;
+      }
+  }
+  return true; 
+}
+
+function assertArraysEqual(arr1,arr2){
+  if (eqArrays(arr1,arr2)) {
+    console.log("✅✅✅ Assertion Passed:" );
+  }
+  else
+    console.log("🛑🛑🛑Assertion Failed:" );
+}
+
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+
+
+
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
+
