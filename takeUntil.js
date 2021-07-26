8e073a5c664117b8fce29f4e7759a0ebbf2969d6
chandni <chandnivaghasia11@gmail.com>
2021-07-26 00:38:54 -0400

    findkey and takeUntil.js changes

--- takeUntil.js
+++ takeUntil.js
@@ -1,21 +1,4 @@
-function eqArrays(arr1, arr2) {
-  if (arr1.length !== arr2.length) return false;
-  for (var i = 0, len = arr1.length; i < len; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-function assertArraysEqual(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log("✅✅✅ Assertion Passed:");
-  }
-  else
-    console.log("🛑🛑🛑Assertion Failed:");
-}
-
+c
 
 const takeUntil = function (array, callback)
 {
